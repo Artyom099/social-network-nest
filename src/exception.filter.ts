078659f8c456a668f1358537ea17d23262efef65
Artyom <artgolubev@bk.ru>
2023-07-07 10:58:20 +0300

    fix exception.filter.ts & main.ts

--- src/exception.filter.ts
+++ src/exception.filter.ts
@@ -16,20 +16,35 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const status = exception.getStatus();
 
     if (status === HttpStatus.BAD_REQUEST) {
-      const errorResponse = {
-        errors: [],
-      };
+      const errorsMessages = [];
       const responseBody: any = exception.getResponse();
-      responseBody.message.forEach((m) =>
-        errorResponse.errors.push({ message: m, field: m.split(' ')[0] }),
-      );
-      response.status(status).json({ errorResponse });
+      //todo можно ли оставить never?
+      responseBody.message.forEach((m: never) => errorsMessages.push(m));
+      response.status(status).json({ errorsMessages });
     } else {
       response.status(status).json({
         statusCode: status,
         timestamp: new Date().toISOString(),
         path: request.url,
       });
+    }
+  }
+}
+
+@Catch(Error)
+export class ErrorExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    if (process.env.environment !== 'production') {
+      response
+        .status(500)
+        .send({ error: exception.toString(), stack: exception.stack });
+    } else {
+      response.status(500).send('some error occurred');
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
-import { HttpExceptionFilter } from './exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
+      transform: true,
       stopAtFirstError: true,
       exceptionFactory: (errors) => {
-        throw new BadRequestException([
-          errors.map((e) => {
-            return {
-              field: e.property,
-            };
-          }),
-        ]);
+        const errorsForResponse: any = [];
+
+        errors.forEach((e) => {
+          const constraintsKeys = Object.keys(e.constraints as {});
+          constraintsKeys.forEach((cKey) => {
+            if (e.constraints) {
+              errorsForResponse.push(e.constraints[cKey]);
+            }
+          });
+        });
+
+        throw new BadRequestException(errorsForResponse);
       },
     }),
   );
-  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
