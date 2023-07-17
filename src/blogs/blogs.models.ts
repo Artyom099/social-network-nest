827f66ff7622c7de83d036d0c0853981592f4e3c
Artyom <artgolubev@bk.ru>
2023-07-17 06:35:57 +0300

    fix blogs.models.ts

--- src/blogs/blogs.models.ts
+++ src/blogs/blogs.models.ts
@@ -1,17 +1,23 @@
-import { IsNotEmpty, IsUrl, Length } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 //todo - не работает валидация данных на пост и пут
 
 export class BlogInputModel {
+  @IsString()
   @IsNotEmpty()
   @Length(3, 15)
+  @Transform(({ value }) => value?.trim())
   name: string;
+  @IsString()
   @IsNotEmpty()
   @Length(3, 500)
+  @Transform(({ value }) => value?.trim())
   description: string;
-  @IsNotEmpty()
   @IsUrl()
+  @IsNotEmpty()
   @Length(3, 100)
+  @Transform(({ value }) => value?.trim())
   websiteUrl: string;
 }
 export type BlogCreateDTO = Omit<BlogViewModel, 'id'>;
--- src/comments/comments.models.ts
+++ src/comments/comments.models.ts
@@ -1,9 +1,12 @@
 import { LikeStatus } from '../utils/constants';
-import { IsNotEmpty, Length } from 'class-validator';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { Transform } from 'class-transformer';
 
 export class CommentInputModel {
+  @IsString()
   @IsNotEmpty()
   @Length(20, 300)
+  @Transform(({ value }) => value?.trim())
   content: string;
 }
 
