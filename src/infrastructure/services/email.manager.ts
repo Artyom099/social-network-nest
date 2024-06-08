e68a9491259da5331ccefd5be544f1f342807c16
Artyom Golubev <artgolubev@bk.ru>
2024-06-08 08:57:22 +0300

    refactor

--- src/infrastructure/services/email.manager.ts
+++ src/infrastructure/services/email.manager.ts
@@ -7,20 +7,24 @@ export class EmailManager {
 
   async sendEmailConfirmationMessage(email: string, code: string) {
     const subject = 'Confirm your email';
+
     const message =
-      ' <h1>Thank for your registration</h1>\n' +
+      ' <h1>Thanks for your registration</h1>\n' +
       ' <p>To finish registration please follow the link below:\n' +
       `     <a href =\'https://somesite.com/confirm-email?code=${code}\'>complete registration</a>\n` +
       ' </p>\n';
-    await this.emailAdapter.sendEmail(email, subject, message);
+
+    return this.emailAdapter.sendEmail(email, subject, message);
   }
 
   async sendEmailRecoveryCode(email: string, code: string) {
     const subject = 'Password recovery';
+
     const message = ` <h1>Password recovery</h1>
       <p>To finish password recovery please follow the link below:
           <a href =\'https://somesite.com/password-recovery?recoveryCode=${code}\'>recovery password</a>
       </p>`;
-    await this.emailAdapter.sendEmail(email, subject, message);
+
+    return this.emailAdapter.sendEmail(email, subject, message);
   }
 }
